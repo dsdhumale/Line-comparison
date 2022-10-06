cd5331bf298f8c1b0db8ebc3a70665cff299a676
dsdhumale <d.sdhumale@gmail.com>
2022-10-07 00:12:05 +0530

    Add : Welcome Message

---
+++ WelcomeMessage.java
@@ -0,0 +1,10 @@
+package com.Bridgelabz.Day8;
+
+public class WelcomeMessage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Welcome to Line Comparison Computation Program");
+	}
+
+}
